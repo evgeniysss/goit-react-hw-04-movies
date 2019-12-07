fc3056f99ccadc984b0353ba4f160f4c5a5708cb
kievtechnoshop@gmail.com <swr83947>
2019-12-07 23:36:17 +0200

    Updates

---
+++ precache-manifest.d55f4b2e97a30d12650eaa56dcb2b7e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d24df53e853912e63f299a7803032b",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "1be43bb455d901030ee8",
+    "url": "/goit-react-hw-04-movies/static/css/main.8a3d2c88.chunk.css"
+  },
+  {
+    "revision": "1dfa38f5680989d64ba8",
+    "url": "/goit-react-hw-04-movies/static/js/2.370cda92.chunk.js"
+  },
+  {
+    "revision": "1be43bb455d901030ee8",
+    "url": "/goit-react-hw-04-movies/static/js/main.08799494.chunk.js"
+  },
+  {
+    "revision": "f6f6d295c662be880dd8",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.b99566bd.js"
+  }
+]);
